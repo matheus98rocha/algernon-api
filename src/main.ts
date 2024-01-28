7315965821f0d7eb8d389b0acaf00a1b6d2bd10a
matheus98rocha <matheus98rocha@gmail.com>
2024-01-28 16:48:03 -0300

    chore: removing not used vars

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configServer = app.get(ConfigService);
-  const port = configServer.get('port');
   await app.listen(3000);
 }
 bootstrap();
